ccd11c82fe110944564bc4583b1cc3924c6f0d6f
Jong-BinPark <68490334+Jong-BinPark@users.noreply.github.com>
2023-09-28 17:58:31 +0900

    2일차 project update

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import MainContent
 # Create your views here.
 
-def index(reqyest):
-    return HttpResponse("Hello World")
+def index(request):
+    content_list = MainContent.objects.order_by('-pub_date')
+    context = {'content_list': content_list}
+    return render(request, 'mysite/content_list.html', context)
